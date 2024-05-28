83b6e496285c8eeb8ca81f1ae86b3c5445f43223
grez72 <grez72@gmail.com>
2024-05-28 05:40:03 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="eval-classification",
+    version="0.1.0",
+    author="George Alvarez",
+    author_email="alvarez@wjh.harvard.edu",
+    description="DNN classification",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/harvard-visionlab/eval-classification",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
