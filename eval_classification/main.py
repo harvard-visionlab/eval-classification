51d4eb9f64252f6bd1043f42ec15b0ac5ae7c8d7
George Alvarez <grez72@gmail.com>
2024-05-30 16:23:44 -0400

    add layer_name

--- eval_classification/main.py
+++ eval_classification/main.py
@@ -1,13 +1,18 @@
 import os
+import math
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
+from scipy.stats import kurtosis
 
 import pandas as pd
+import numpy as np
 from fastprogress import progress_bar
 from collections import defaultdict
 
 from pdb import set_trace
+
+from .utils import snr
 
 @torch.no_grad()    
 def accuracy(output, target, topk=(1,)):
@@ -15,7 +20,7 @@ def accuracy(output, target, topk=(1,)):
     maxk = max(topk)
     batch_size = target.size(0)
 
-    _, pred = output.topk(maxk, 1, True, True)
+    act, pred = output.topk(maxk, 1, True, True)
     pred = pred.t()
     correct = pred.eq(target.view(1, -1).expand_as(pred))
 
@@ -25,10 +30,11 @@ def accuracy(output, target, topk=(1,)):
         correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
         res.append(correct_k.mul_(100.0 / batch_size))
         corrects.append(correct[:k].any(dim=0).float())
-    return pred, corrects, res
+    return pred, act, corrects, res
     
 @torch.no_grad()
-def validation(model, dataset, topk=(1,5), map_output=None, batch_size=250, num_workers=10, shuffle=False, pin_memory=True, mb=None):
+def validation(model, dataset, layer_names=None, topk=(1,5), map_output=None, batch_size=250, num_workers=10, 
+               shuffle=False, pin_memory=True, mb=None):
     device = next(model.parameters()).device
     criterion = nn.CrossEntropyLoss(reduction='none')
     filepaths = [(os.path.sep).join(f.split(os.path.sep)[-2:]) for f,_ in dataset.imgs]
@@ -36,7 +42,7 @@ def validation(model, dataset, topk=(1,5), map_output=None, batch_size=250, num_
                             shuffle=shuffle, pin_memory=pin_memory)
     
     model.eval()
-    results = defaultdict(list)
+    results = defaultdict(lambda: defaultdict(list))
     count = 0
     for i, batch in enumerate(progress_bar(dataloader, parent=mb)):
         batch_size = batch[0].shape[0]
@@ -44,33 +50,60 @@ def validation(model, dataset, topk=(1,5), map_output=None, batch_size=250, num_
         target = batch[1].to(device, non_blocking=True)
         index = batch[2].tolist()
         filenames = [filepaths[idx] for idx in index]
+        
+        if layer_names is None:
+            output = model(images)
+            if map_output is not None:
+                output = map_output(output)
+            features = dict(output=output)
+        else:
+            with FeatureExtractor(model, layer_names) as extractor:
+                features = extractor(images)
+                
+        for layer_name, output in features.items():
+            loss = criterion(output, target)
 
-        output = model(images)
-        if map_output is not None:
-            output = map_output(output)
+            preds, act, accuracies, _ = accuracy(output, target, topk=topk)     
+
+            # maximum target and non-target activation
+            target_activations = torch.gather(output, 1, target.view(-1,1)).squeeze()
+            mask = torch.ones_like(output, dtype=bool).scatter_(1, target.view(-1, 1), False)
+            non_target_output = output[mask].view(output.shape[0], -1)
+            max_nontarget_activation = non_target_output.max(dim=1).values
+            snr = abs((target_activations-max_nontarget_activation) / math.sqrt(2))
             
-        loss = criterion(output, target)
+            results[layer_name]['layer_name'] += [layer_name] * len(index)
+            results[layer_name]['index'] += index            
+            results[layer_name]['filenames'] += filenames
+            results[layer_name]['target_label'] += target.tolist()        
+            results[layer_name]['predicted_label'] += preds[0].tolist()
+            results[layer_name]['top5_labels'] += preds.t().tolist()
+            results[layer_name]['top5_outputs'] += act.tolist()
+            results[layer_name]['target_act'] += target_activations.tolist()
+            results[layer_name]['max_nontarget_act'] += max_nontarget_activation.tolist()
+            results[layer_name]['snr'] += snr.tolist()
+            results[layer_name]['loss'] += loss.tolist()
 
-        preds, accuracies, _ = accuracy(output, target, topk=topk)     
-        
-        # maximum target and non-target activation
-        target_activations = torch.gather(output, 1, target.view(-1,1)).squeeze()
-        mask = torch.ones_like(output, dtype=bool).scatter_(1, target.view(-1, 1), False)
-        non_target_output = output[mask].view(output.shape[0], -1)
-        max_nontarget_activation = non_target_output.max(dim=1).values
-        snr = target_activations / max_nontarget_activation
-        
-        results['index'] += index
-        results['filenames'] += filenames
-        results['target_label'] += target.tolist()
-        results['loss'] += loss.tolist()
-        results['predicted_label'] += preds[0].tolist()
-        results['target_act'] += target_activations.tolist()
-        results['max_nontarget_act'] += max_nontarget_activation.tolist()
-        results['snr'] += snr.tolist()
-        
-        for idx,k in enumerate(topk):
-            results[f'correct{k}'] += accuracies[idx].tolist()
+            for idx,k in enumerate(topk):
+                results[layer_name][f'correct{k}'] += accuracies[idx].tolist()
 
-    df = pd.DataFrame(results)
-    return df, None
+    # full image-by-image results 
+    dfs = dict()
+    summary = dict()
+    for layer_name,res in results.items():
+        df = pd.DataFrame(res)
+        dfs[layer_name] = df
+    
+        # leaderboard summary    
+        summary[layer_name] = dict(
+            top1=df.correct1.mean()*100,
+            top5=df.correct5.mean()*100,
+            snr_mean=df.snr.mean(),
+            snr_sqrt_mean_squared=np.sqrt(df.snr).mean()**2,
+            snr_min=df.snr.min(),
+            snr_max=df.snr.max(),
+            snr_kurtosis=kurtosis(df.snr),
+            snr_sqrt_kurtosis=kurtosis(np.sqrt(df.snr))
+        )
+    
+    return dfs, summary
---
+++ eval_classification/utils.py
@@ -0,0 +1,13 @@
+import math
+
+def snr(act1, act2):
+    return shortest_distance_to_unit_line(act1, act2)
+
+def shortest_distance_to_unit_line(xi, yi):
+    '''Calculate the distance between the point (xi, yi) and the line x=y
+        distance point (x0,y0) to line (ax + by + c = 0):
+        abs(a * x0 + b * y0 + c) / sqrt(a^2 + b^2)
+        https://www.mathportal.org/calculators/analytic-geometry/line-point-distance.php
+    '''
+    distance = abs((xi-yi) / math.sqrt(2))
+    return distance
