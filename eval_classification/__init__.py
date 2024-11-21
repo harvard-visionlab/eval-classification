3df9dcf2517702baffe6fd8d3792cb0eed50eb67
George Alvarez <grez72@gmail.com>
2024-11-21 06:03:34 -0500

    more generic dataloading

--- eval_classification/__init__.py
+++ eval_classification/__init__.py
@@ -1,3 +1,3 @@
 from .main import validation
-from .logging import ResultsLogger
+# from .logging import ResultsLogger
 from .version import __version__
--- eval_classification/main.py
+++ eval_classification/main.py
@@ -2,12 +2,12 @@ import os
 import math
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader
+from torch.utils.data import Dataset, DataLoader
 from scipy.stats import kurtosis
 
 import pandas as pd
 import numpy as np
-from fastprogress import progress_bar
+from tqdm import tqdm
 from collections import defaultdict
 
 from pdb import set_trace
@@ -15,6 +15,43 @@ from pdb import set_trace
 from .utils import snr
 from .feature_extractor import FeatureExtractor
 
+class DatasetWrapper(Dataset):
+    def __init__(self, dataset):
+        """
+        Wraps a dataset to include index and file paths (optional).
+        
+        Args:
+            dataset (Dataset): The original dataset.
+            filepaths (list or None): A list of file paths corresponding to dataset samples.
+        """
+        self.dataset = dataset
+        self.filepaths = self.get_filepaths(dataset)
+    
+    def get_filepaths(self, dataset):
+        imgs = (dataset.imgs if hasattr(dataset, "imgs") 
+                else dataset.samples if hasattr(dataset, "samples") 
+                else dataset._samples if hasattr(dataset, "_samples")
+                else [])
+        filepaths = [(os.path.sep).join(f.split(os.path.sep)[-3:]) for f,_ in imgs]
+
+        return filepaths
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, index):
+        sample = self.dataset[index]
+        if isinstance(sample, dict):
+            image = sample['image']
+            label = sample['label']
+            path = sample['path']
+        else:
+            image = sample[0]
+            label = sample[1]
+            path = self.filepaths[index] if self.filepaths else None
+            
+        return image, label, index, path
+    
 @torch.no_grad()    
 def accuracy(output, target, topk=(1,)):
     """Computes the accuracy over the k top predictions for the specified values of k"""
@@ -36,25 +73,22 @@ def accuracy(output, target, topk=(1,)):
 @torch.no_grad()
 def validation(model, dataset, layer_names=None, topk=(1,5), map_output=None, 
                batch_size=250, num_workers=len(os.sched_getaffinity(0)) - 1, 
-               shuffle=False, pin_memory=True, default_output_name='output', meta={}, mb=None):
+               shuffle=False, pin_memory=True, default_output_name='output', meta={}):    
     device = next(model.parameters()).device
     criterion = nn.CrossEntropyLoss(reduction='none')
-    imgs = (dataset.imgs if hasattr(dataset, "imgs") 
-            else dataset.samples if hasattr(dataset, "samples") 
-            else dataset._samples)
-    filepaths = [(os.path.sep).join(f.split(os.path.sep)[-2:]) for f,_ in imgs]
+    dataset = DatasetWrapper(dataset)
     dataloader = DataLoader(dataset, batch_size=batch_size, num_workers=num_workers, 
                             shuffle=shuffle, pin_memory=pin_memory)
     
     model.eval()
     results = defaultdict(lambda: defaultdict(list))
     count = 0
-    for i, batch in enumerate(progress_bar(dataloader, parent=mb)):
-        batch_size = batch[0].shape[0]
+    for i, batch in enumerate(tqdm(dataloader)):
         images = batch[0].to(device, non_blocking=True)
         target = batch[1].to(device, non_blocking=True)
         index = batch[2].tolist()
-        filenames = [filepaths[idx] for idx in index]
+        filenames = batch[3]
+        batch_size = images.shape[0]
         
         if layer_names is None:
             output = model(images)
@@ -76,7 +110,7 @@ def validation(model, dataset, layer_names=None, topk=(1,5), map_output=None,
             mask = torch.ones_like(output, dtype=bool).scatter_(1, target.view(-1, 1), False)
             non_target_output = output[mask].view(output.shape[0], -1)
             max_nontarget_activation = non_target_output.max(dim=1).values
-            snr = (target_activations-max_nontarget_activation) / math.sqrt(2)
+            decision_margin = (target_activations-max_nontarget_activation) / math.sqrt(2)
             
             results[layer_name]['layer_name'] += [layer_name] * len(index)
             results[layer_name]['index'] += index            
@@ -87,7 +121,7 @@ def validation(model, dataset, layer_names=None, topk=(1,5), map_output=None,
             results[layer_name]['top5_outputs'] += act.tolist()
             results[layer_name]['target_act'] += target_activations.tolist()
             results[layer_name]['max_nontarget_act'] += max_nontarget_activation.tolist()
-            results[layer_name]['snr'] += snr.tolist()
+            results[layer_name]['decision_margin'] += decision_margin.tolist()
             results[layer_name]['loss'] += loss.tolist()
 
             for idx,k in enumerate(topk):
@@ -104,10 +138,10 @@ def validation(model, dataset, layer_names=None, topk=(1,5), map_output=None,
         summary = dict(**meta, **dict(
             top1=df.correct1.mean()*100,
             top5=df.correct5.mean()*100,
-            snr_mean=df.snr.mean(),
-            snr_min=df.snr.min(),
-            snr_max=df.snr.max(),
-            snr_kurtosis=kurtosis(df.snr),
+            dm_mean=df.decision_margin.mean(),
+            dm_min=df.decision_margin.min(),
+            dm_max=df.decision_margin.max(),
+            dm_kurtosis=kurtosis(df.decision_margin),
         ))
         summary = pd.DataFrame(summary, index=[0])
         
